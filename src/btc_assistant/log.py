161b94b08d7b1f7a48b7a0b513715cb6c4a13db3
Neville Tummon <nt.devs@gmail.com>
2018-04-22 13:24:56 +1000

    forgot to override root logger log level

--- src/btc_assistant/log.py
+++ src/btc_assistant/log.py
@@ -2,16 +2,20 @@ import logging
 from os import environ
 
 LOGLEVEL = environ.get("LOGLEVEL", "INFO")
-level_nr = logging.getLevelName(LOGLEVEL)
 
 def _set_root_logger_level():
-    logging.basicConfig(level=level_nr)
+    logging.info("Logger level is set to: {}".format(logging.INFO))
+    logging.basicConfig(
+        level=LOGLEVEL,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    logging.getLogger().setLevel(LOGLEVEL)
     logging.getLogger('botocore.vendored').setLevel(logging.WARNING)
     # add any extra namespaces to override the default configuration
 
 def get_logger(namespace):
     logger = logging.getLogger(namespace)
-    logger.setLevel(level_nr)
+    logger.setLevel(LOGLEVEL)
     return logger
 
 _set_root_logger_level()
