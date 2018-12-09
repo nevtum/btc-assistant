53a0dd956557aacf5b7baff8a5413100d89a3f7a
Neville Tummon <nt.devs@gmail.com>
2018-12-09 21:44:43 +1100

    must pass unix timestamp to get market data

--- src/web_app/presentation/api/views.py
+++ src/web_app/presentation/api/views.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from factory import create_market_data_gateway
 from flask_restplus import Resource
@@ -11,10 +11,14 @@ logger.setLevel(logging.INFO)
 
 gw = create_market_data_gateway()
 
-@ns.route('/data')
+@ns.route("/data/<float:unix_timestamp>")
+@ns.doc(params={'unix_timestamp': '86400.0'})
 class BitcoinMarketList(Resource):
-    def get(self):
-        resp = gw.get_bitcoin_market_data(datetime.utcnow() - timedelta(hours=24))
+    def get(self, unix_timestamp):
+        dt_ref = datetime.fromtimestamp(unix_timestamp)
+        resp = gw.get_bitcoin_market_data(dt_ref)
         return {
+            "count": len(resp["Items"]),
+            "next_page_url": None,
             "results": resp["Items"]
         }, 200
