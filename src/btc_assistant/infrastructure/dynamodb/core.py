7046b1072a769a25998ddff214c102250cf579ff
Neville Tummon <neville.tummon@isentia.com>
2020-05-17 00:16:28 +1000

    add structured logging

--- src/btc_assistant/infrastructure/dynamodb/core.py
+++ src/btc_assistant/infrastructure/dynamodb/core.py
@@ -10,14 +10,12 @@ _client = boto3.client("dynamodb")
 
 
 def execute_query(**kwargs):
-    logger.info(kwargs)
     resp = _client.query(**kwargs)
     logger.info("Consumed capacity: {}".format(resp["ConsumedCapacity"]))
     return resp
 
 
 def execute_insert(**kwargs):
-    logger.info(kwargs)
     resp = _client.put_item(ReturnConsumedCapacity="TOTAL", **kwargs)
     logger.info("Consumed capacity: {}".format(resp["ConsumedCapacity"]))
     return resp
