bef8a6d13d4f47049dc44bd4cb9aed92029b10aa
Neville Tummon <neville.tummon@isentia.com>
2020-05-16 23:45:15 +1000

    change worker to write to new table schema

--- src/btc_assistant/index.py
+++ src/btc_assistant/index.py
@@ -1,19 +1,8 @@
-from os import environ
+from app import WorkerApp
 
-import factory
-from log import get_logger
+app = WorkerApp()
 
-logger = get_logger(__name__)
+lambda_handler = app.create_handler()
 
-def lambda_handler(event, context):
-    logger.debug("Event payload: {}".format(event))
-    logger.debug("Context: {}".format(context))
-    
-    checker = factory.create_price_checker()
-    storage = factory.create_storage()
-    data = checker.get_btc_day_market_data('AUD')
-    storage.store_record(data)
-    return "Success"
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     lambda_handler(None, None)
--- src/btc_assistant/infrastructure/network/services.py
+++ src/btc_assistant/infrastructure/network/services.py
@@ -1,6 +1,7 @@
 import json
 
 from dateutil import parser
+
 from log import get_logger
 
 from .http_utils import RESTClient
@@ -9,17 +10,19 @@ from .urls import IndependentReserveUrls
 
 logger = get_logger(__name__)
 
+
 class BTCPriceChecker:
     def get_btc_day_market_data(self, currency_code):
         logger.debug("Getting BTC market data from Independent Reserve...")
-        if currency_code.lower() not in ['aud', 'usd']:
+        if currency_code.lower() not in ["aud", "usd"]:
             raise AttributeError("Must specify either 'aud' or 'usd' currency code")
-        url = IndependentReserveUrls.market_data_url('xbt', currency_code.lower())
+        url = IndependentReserveUrls.market_data_url("xbt", currency_code.lower())
         a_dict = RESTClient.get(url).json()
         logger.info("Data retrieved: {}".format(a_dict))
         return BitcoinMarketData(
-            timestamp=parser.parse(a_dict['CreatedTimestampUtc']),
-            price=a_dict['LastPrice'],
-            volume=a_dict['DayVolumeXbt'],
-            url="www.independentreserve.com"
+            timestamp=parser.parse(a_dict["CreatedTimestampUtc"]),
+            price=a_dict["LastPrice"],
+            volume=a_dict["DayVolumeXbt"],
+            url="www.independentreserve.com",
+            symbol=f"BTC{currency_code.upper()}",
         )
