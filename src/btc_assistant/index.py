3400c181ff71d6e222312c510095ae8c66582d7f
Neville Tummon <neville.tummon@isentia.com>
2020-05-16 23:59:26 +1000

    clean up

--- src/btc_assistant/index.py
+++ src/btc_assistant/index.py
@@ -1,5 +1,7 @@
 from app import WorkerApp
+from log import configure_root_logger_level
 
+configure_root_logger_level()
 app = WorkerApp()
 
 lambda_handler = app.create_handler()
--- src/btc_assistant/log.py
+++ src/btc_assistant/log.py
@@ -3,19 +3,18 @@ from os import environ
 
 LOGLEVEL = environ.get("LOGLEVEL", "INFO")
 
-def _set_root_logger_level():
+
+def configure_root_logger_level():
     logging.info("Logger level is set to: {}".format(logging.INFO))
     logging.basicConfig(
-        level=LOGLEVEL,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        level=LOGLEVEL, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
     logging.getLogger().setLevel(LOGLEVEL)
-    logging.getLogger('botocore.vendored').setLevel(logging.WARNING)
+    logging.getLogger("botocore.vendored").setLevel(logging.WARNING)
     # add any extra namespaces to override the default configuration
+
 
 def get_logger(namespace):
     logger = logging.getLogger(namespace)
     logger.setLevel(LOGLEVEL)
     return logger
-
-_set_root_logger_level()
