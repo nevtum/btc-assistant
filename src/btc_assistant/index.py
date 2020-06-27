1995056276f0c640b72cc12f6f8d933994f8404d
Neville Tummon <neville.tummon@isentia.com>
2020-06-27 23:36:35 +1000

    add ssm and other various improvements

--- src/btc_assistant/index.py
+++ src/btc_assistant/index.py
@@ -9,4 +9,4 @@ ENVIRONMENT = environ.get("ENV", "local")
 lambda_handler = AppFactory(ENVIRONMENT).create_handler()
 
 if __name__ == "__main__":
-    lambda_handler(None, None)
+    lambda_handler({}, None)
--- src/btc_assistant/log.py
+++ src/btc_assistant/log.py
@@ -8,16 +8,16 @@ LOGLEVEL = environ.get("LOGLEVEL", "INFO")
 
 def configure_root_logger_level():
     logger = logging.getLogger()
-    logger.setLevel(LOGLEVEL)
+
+    for h in logger.handlers:
+        logger.removeHandler(h)
 
     handler = logging.StreamHandler()
     formatter = jsonlogger.JsonFormatter("%(asctime)s %(name)s %(levelname)s %(message)s")
-    # formatter = jsonlogger.JsonFormatter()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-
-    logging.basicConfig(level=LOGLEVEL)
-    logging.info(f"Logger level is set to: {LOGLEVEL}")
+    logger.setLevel(LOGLEVEL)
+    logging.info(f"Logger level is set", extra={"log_level": LOGLEVEL})
 
     logging.getLogger("botocore.vendored").setLevel(logging.WARNING)
     # add any extra namespaces to override the default configuration
